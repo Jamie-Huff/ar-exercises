e5d866bc26a6804fb0dcf1c3314f3bf80db7be02
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-08-18 19:10:35 +0000

    finished ex 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,3 +9,15 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 # Your code goes here ...
+
+# Employee.create( store_id: 1, first_name:'Jamie', last_name:'huff', hourly_rate:190 )
+puts 'Whats your store bitchass'
+whatyosto = gets.chomp
+puts 'How much money you got'
+whatyomuny = gets.chomp
+
+new_store = Store.create( name: whatyosto, annual_revenue: whatyomuny )
+
+new_store.errors.messages.each do |key, value|
+  puts "#{key}: #{value}"
+end
